47f7c6e641885d7683753b6f8cfa3e55ba7370c7
nicksony <xylol.eli@mail.com>
2024-10-28 10:25:57 +0200

    meow

---
+++ yl/yl7.py
@@ -0,0 +1,9 @@
+# Kirjuta programm, mis ütleb, kas kasutaja poolt etteantud täisarv on paarisarv või mitte. 
+
+num = int(input ('Enter any number to test whether it is odd or even: '))
+
+if (num % 2) == 0:
+     print ('The number is even')
+
+else:
+    print ('The provided number is odd')
