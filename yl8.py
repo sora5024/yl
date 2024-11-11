244db50ee24a89422656d6d20e29b21826e5a91c
nicksony <xylol.eli@gmail.com>
2024-11-11 09:28:05 +0200

    commit

---
+++ yl8.py
@@ -0,0 +1,8 @@
+# Kirjutada programm, mis kontrollib, kas antud positiivne täisarv on liig- või lihtaasta number.
+# Aasta on liigaasta kui ta jagub neljasajaga või jagub neljaga ja ei jagu sajaga.
+
+number = 1
+
+while number <= 5:
+    print(number)
+    number += 1
---
+++ yl9.py
@@ -0,0 +1,5 @@
+# Kolmnurki liigitatakse külgede pikkuse järgi erikülgseteks, võrdhaarseteks ja võrdkülgseteks. 
+# Kirjutada programm, mis küsib kasutajalt kolme külje pikkused ning väljastab vastusena kolmnurga liigi.
+# Lisaks tuleb kontrollida, kas etteantud küljepikkustega kolmnurk saab üldse eksisteerida.
+# Külje pikkused ei pea olema täisarvud.
+
