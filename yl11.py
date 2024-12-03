5feba948ecff09b973e4001d334290d081b17ada
sora <105000031+sora5024@users.noreply.github.com>
2024-12-03 09:29:41 +0200

    commit

---
+++ yl11.py
@@ -0,0 +1,6 @@
+# Kirjuta programm, mis küsib kasutajalt sisendina stringi.
+# Eemalda selle sisendi algusest ja lõpust tühikud.
+# String peab vastama tingimustele, et selles on vähemalt seitse sümbolit ja et sümbolite arv on paarituarvuline.
+# Väljasta selle stringi kolm keskmist sümbolit.
+
+txt = input('Sisesta tekst')
