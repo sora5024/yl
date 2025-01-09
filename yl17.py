66396e1c50ee9ba1273ba91b3ca271cdf68abca1
sora <105000031+sora5024@users.noreply.github.com>
2025-01-09 00:19:11 -0800

    commit

---
+++ yl17.py
@@ -0,0 +1,6 @@
+# Väljasta korduslause abil numbrid 1..5 (kodruslause - loop, while)
+
+i = 1
+while i <= 5:
+    print(1)
+    i += 1
---
+++ yl18.py
@@ -0,0 +1,7 @@
+# Väljasta korduslause abil numbrid 5..1 (for in, range)
+
+for el in range(5, 0, -1):
+    print(el)
+
+for el in range(0, 5, 100, 5):
+    print(el)
