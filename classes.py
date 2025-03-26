5f88cd3062b2026e31bc81f565ac06b8c9ab4f6d
sora <105000031+sora5024@users.noreply.github.com>
2025-03-26 13:06:56 +0200

    commit

--- classes.py
+++ classes.py
@@ -39,3 +39,17 @@ if student_one == student_two:
     print('Student 1 is the same as student 2.')
 else:
     print('Student 1 is not equivalent to student 2.')
+
+print(f'1. opilane: {student_one.first_name} {student_one.last_name} - {student_one.school}')
+print(f'2. opilane: {student_two.first_name} {student_two.last_name} - {student_two.school}')
+print(f'3. opilane: {adult_student.first_name} {adult_student.last_name} - {adult_student.school}')
+student_two.add_reason_for_absence('Medical reasons')
+
+#print(student_one._phone)
+
+print(student_one.get_phone())
+student_one.set_phone('52123456')
+print(student_one.get_phone())
+
+print(f'{adult_student.first_name} Telephone is {adult_student.get_phone()}')
+adult_student.print_occupation()
